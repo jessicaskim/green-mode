5e3bcec33d0b3e59e66486f088d9a020284dcd6e
Jessica Kim <sk4711@columbia.edu>
2020-08-28 21:20:59 +0900

    Colored basic HTML elements with shades of green
    Has to fix input placeholder text color

---
+++ script.js
@@ -0,0 +1,98 @@
+let body = document.getElementsByTagName('body')[0];
+body.style['background-color'] = '#CBDEC6';
+body.style['border'] = '0.3px solid #ABD3A0';
+
+let bigHeaders = document.getElementsByTagName('h1');
+for (elem of bigHeaders) {
+    elem.style['color'] = '#008000';
+    elem.style['border'] = '0.3px solid #ABD3A0';
+
+}
+
+let medHeaders = document.getElementsByTagName('h2');
+for (elem of medHeaders) {
+    elem.style['color'] = '#468733';
+    elem.style['border'] = '0.3px solid #ABD3A0';
+
+}
+
+let smallHeaders = document.getElementsByTagName('h3');
+for (elem of medHeaders) {
+    elem.style['color'] = '#588749';
+    elem.style['border'] = '0.3px solid #ABD3A0';
+
+}
+
+let tinyHeaders = document.getElementsByTagName('h4');
+for (elem of tinyHeaders) {
+    elem.style['color'] = '#4A723E';
+    elem.style['border'] = '0.3px solid #ABD3A0';
+
+}
+
+let header = document.getElementsByTagName('header');
+for (elem of header) {
+    elem.style['background-color'] = '#B0D6A6';
+    elem.style['border'] = '1px solid #ABD3A0';
+}
+
+let divs = document.getElementsByTagName('div');
+for (elem of divs) {
+    elem.style['background-color'] = '#D0E1CB';
+    elem.style['color'] = '#285C1B';
+    // elem.style['border'] = '0.3px solid #ABD3A0';
+}
+
+let spans = document.getElementsByTagName('span');
+for (elem of spans) {
+    elem.style['border'] = '0.3px solid #ABD3A0';
+}
+
+let paragraphs = document.getElementsByTagName('p');
+for (elem of paragraphs) {
+    elem.style['color'] = '#285C1B';
+}
+
+let lists = document.getElementsByTagName('li');
+for (elem of lists) {
+    elem.style['color'] = '#285C1B';
+}
+
+let links = document.getElementsByTagName('a');
+for (elem of links) {
+    elem.style['color'] = '#285C1B';
+    elem.style['border'] = '2px solid #ABD3A0';
+
+}
+
+let tables = document.getElementsByTagName('table');
+for (elem of tables) {
+    elem.style['background-color'] = '#B5D1AD';
+    elem.style['color'] = '#285C1B';
+    elem.style['border'] = '0.3px solid #ABD3A0';
+
+}
+
+let tableBody = document.getElementsByTagName('th');
+for (elem of tableBody) {
+    elem.style['color'] = '#285C1B';
+}
+
+// let buttons = document.getElementsByTagName('button');
+// for (elem of buttons) {
+//     elem.style['border'] = '0.3px solid #ABD3A0';
+
+// }
+// let tableElem = document.getElementsByTagName('td');
+// for (elem of tableElem) {
+//     elem.style['color'] = '#285C1B';
+// }
+
+// let inputs = document.getElementsByTagName('input');
+// for (elem of inputs) {
+//     elem.style['background-color'] = '#B5D1AD';
+//     let placeholder = elem.placeholder;
+//     placeholder.style['color'] = '#285C1B';
+// }
+
+
